5919d4e8b0f52974ff36c2a71fcac2ca8fc8a6da
shhong <>
2018-08-28 11:00:28 +0900

    Add crown-and-anchor

--- crown-and-anchor.js
+++ crown-and-anchor.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
     'user strict';
 
-    let funds = 50;
+    let funds = 50; // 시작 조건
+    let round = 0;
+
+    console.log("funds: " + funds);
+
     while(funds > 1 && funds < 100) {
+        round++;
+        console.log(`round ${round}`);
+        console.log(`\tstarting funds: ${funds}p`);
+
         // 돈을 겁니다.
         const bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
-        console.log("funds: " + funds);
 
         let totalBet = rand(1, funds);
         console.log("totalBet: " + totalBet);
@@ -23,16 +30,26 @@ $(document).ready(function() {
                 remaining = remaining - bet;
             } while (remaining > 0);
         }
+        funds = funds - totalBet;
+        console.log('\tbets: ' + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(', ') + ` (total: ${totalBet} penced)`);
 
         // 주사위를 굴립니다.
         const hand = [];
         for(let roll = 0; roll < 3; roll++) {
             hand.push(randFace());
         }
+        console.log(`\thand: ${hand.join(', ')}`);
 
         // 그림을 맞추면 돈을 가져옵니다.
-        funds = funds - totalBet;
+        let winnings = 0;
+        for(let die = 0; die < hand.length; die++) {
+            let face = hand[die];
+            if(bets[face] > 0) winnings = winnings + bets[face];
+        }
+        funds = funds + winnings;
+        console.log(`\twinnings: ${winnings}`);
     }   
+    console.log(`\tending funds: ${funds}`);
 
     function rand(m, n) {
         return m + Math.floor((n - m + 1) * Math.random());
