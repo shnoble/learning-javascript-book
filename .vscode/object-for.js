589ab0e98b607fe4ff2eb80bb61991f6924e2a6b
shhong <shhong@nhnent.com>
2018-09-07 12:26:29 +0900

    Add object-for.js

---
+++ .vscode/object-for.js
@@ -0,0 +1,7 @@
+const SYM = Symbol();
+const o = { a: 1, b: 2, c: 3, [SYM]: 4};
+Object.keys(o).forEach(prop => console.log(`${prop}: ${o[prop]}`));
+
+const o1 = { applie:1, xochitl: 2, balloon: 3, guitar: 4, xylophone: 5 };
+Object.keys(o1).filter(prop => prop.match(/^x/)).forEach(prop => console.log(`${prop}: ${o1[prop]}`));
+
